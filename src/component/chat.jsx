54e8ac094c50c5aed21ed4c16b6aa3b0ea1e33d6
chunjaemin <chun4582@naver.com>
2024-02-12 02:28:35 +0900

    chat-design

--- src/component/chat.jsx
+++ src/component/chat.jsx
@@ -5,48 +5,52 @@ import {start, addChat} from '../js/client.js'
 
 export default function chatRoom() {
 
-    let {id} = useParams();
+    let { id } = useParams();
     let [chat, changeChat] = useState([])
     let [message, changeMessage] = useState('')
 
-    useEffect(()=>{
+    useEffect(() => {
 
         let socket = start(id);
 
         socket.on('message', (msg) => {
-            changeChat((chat)=> [...chat, msg])
+            changeChat((chat) => [...chat, msg])
         })
-    },[])
+    }, [])
 
     return (
         <>
-            <div className='main-container'>
-                <div className='chat-flex-container'>
-                    <div id='chat-box' className='chat-flex-container-left'>
-                        {
-                            chat.map((element)=>{
-                                return <li>{element}</li>
-                            })
-                        }
-                    </div>
-                    <div className='chat-flex-container-right'>
+            <div className='chat-main-container'>
+                <div className='chat-container'>
+                    <div className='chat-flex-container'>
+                        <div id='chat-box' className='chat-flex-container-left'>
+                            {
+                                chat.map((element) => {
+                                    return <li>{element}</li>
+                                })
+                            }
+                        </div>
+                        <div className='chat-flex-container-right'>
 
+                        </div>
+                    </div>
+                    <div className='input-container'>
+                        <input placeholder='메세지를 입력해주세요'
+                            className='chat-input'
+                            value={message}
+                            onInput={(e) => { changeMessage(e.target.value) }}
+                            onKeyDown={(e) => {
+                                if (e.code === 'Enter') {
+                                    if (e.nativeEvent.isComposing == false) {
+                                        addChat(message);
+                                        changeChat((chat) => [...chat, message])
+                                        changeMessage('')
+                                    }
+                                }
+                            }}>
+                        </input>
                     </div>
                 </div>
-
-                <input placeholder='메세지를 입력해주세요'
-                    value={message}
-                    onInput={(e) => {changeMessage(e.target.value) }}
-                    onKeyDown={(e) => {
-                        if (e.code === 'Enter') {
-                            if (e.nativeEvent.isComposing == false) {
-                                addChat(message);
-                                changeChat((chat)=> [...chat, message])
-                                changeMessage('')
-                            }
-                        }
-                    }}>
-                </input>
             </div>
         </>
     )
