3c6530ca9a71fccc8a1ecc2f89468629ef0dae9d
chunjaemin <chun4582@naver.com>
2024-02-19 02:40:50 +0900

    rtc-almost-clear

--- src/component/faceChat.jsx
+++ src/component/faceChat.jsx
@@ -11,120 +11,123 @@ export default function FaceChat (props) {
     let htmlCount = useRef(0)
     let [sound, setSound] = useState(true)
     let [monitor, setMonitor] = useState(true)
-    let redBtn = 'red-btn'
-    let soundBtn = 'sound-btn'
     
     let navigate = useNavigate();
 
-    useEffect(async ()=>{ 
-
-        if (!myStream.current) {
-            await getMedia();
-        }
-
-        socket.on('socket-id', async (id)=>{
-            socket.emit('socket-id', id)
-        })
-
-        socket.on('get-userId', async (id)=>{
+    useEffect(()=>{ 
+        async function start() {
             if (!myStream.current) {
                 await getMedia();
             }
-            //상대방의 id를 받았을 때 할 행동
-            createConnection(id);
-            await createOffer(id);
-        })
-        
-        socket.on('offer', async (offer, id)=>{
-            if (!myStream.current) {
-                await getMedia();
-            }
-            //이 코드가 여러번 실행됨
-            createConnection(id);
-            rtcObject.current[id].setRemoteDescription(offer);
-            const answer = await rtcObject.current[id].createAnswer();
-            rtcObject.current[id].setLocalDescription(answer);
-            socket.emit('answer', answer, id);
-        })
 
-        socket.on('answer', (answer, id)=>{
-            rtcObject.current[id].setRemoteDescription(answer);
-        })
-    
-        socket.on('ice', (icecandidate, id) => {
-            if (rtcObject.current[id]) {
-                rtcObject.current[id].addIceCandidate(icecandidate);
-            }
-        })
+            socket.on('socket-id', async (id) => {
+                socket.emit('socket-id', id)
+            })
 
-        socket.on('leave-rtc',(id)=>{
-            const videoTag = document.getElementById(htmlObjectId.current[id]);
-            videoTag.remove();
-            rtcObject.current[id].close();
-        })
-        
-        
-        async function getMedia() {
-            try {
-                myStream.current = await navigator.mediaDevices.getUserMedia({
-                    audio: false,
-                    video: true,
-                });
-                videoSelf.current.srcObject = myStream.current
+            socket.on('get-userId', async (id) => {
+                if (!myStream.current) {
+                    await getMedia();
+                }
+                //상대방의 id를 받았을 때 할 행동
+                createConnection(id);
+                await createOffer(id);
+            })
 
-            } catch (e) {
-                console.log(e);
+            socket.on('offer', async (offer, id) => {
+                if (!myStream.current) {
+                    await getMedia();
+                }
+                //이 코드가 여러번 실행됨
+                createConnection(id);
+                rtcObject.current[id].setRemoteDescription(offer);
+                const answer = await rtcObject.current[id].createAnswer();
+                rtcObject.current[id].setLocalDescription(answer);
+                socket.emit('answer', answer, id);
+            })
+
+            socket.on('answer', (answer, id) => {
+                rtcObject.current[id].setRemoteDescription(answer);
+            })
+
+            socket.on('ice', (icecandidate, id) => {
+                if (rtcObject.current[id]) {
+                    rtcObject.current[id].addIceCandidate(icecandidate);
+                }
+            })
+
+            socket.on('leave-rtc', (id) => {
+                const videoTag = document.getElementById(htmlObjectId.current[id]);
+                videoTag.remove();
+                rtcObject.current[id].close();
+            })
+
+
+            async function getMedia() {
+                try {
+                    myStream.current = await navigator.mediaDevices.getUserMedia({
+                        audio: true,
+                        video: true,
+                    });
+                    videoSelf.current.srcObject = myStream.current
+
+                } catch (e) {
+                    console.log(e);
+                }
             }
-        }
-        
-        function createConnection (id) {
-            htmlObjectId.current[id] = htmlCount.current;
-            htmlCount.current += 1
-            rtcObject.current[id] = new RTCPeerConnection({
-                iceServers: [
-                    {
-                        urls: "stun:stun.l.google.com:19302",
-                        urls: "stun:stun1.l.google.com:19302",
-                        urls: "stun:stun2.l.google.com:19302",
-                        urls: "stun:stun3.l.google.com:19302",
-                        urls: "stun:stun4.l.google.com:19302",
+
+            function createConnection(id) {
+                htmlObjectId.current[id] = htmlCount.current;
+                htmlCount.current += 1
+                rtcObject.current[id] = new RTCPeerConnection({
+                    iceServers: [
+                        {
+                            urls: "stun:stun.l.google.com:19302",
+                            urls: "stun:stun1.l.google.com:19302",
+                            urls: "stun:stun2.l.google.com:19302",
+                            urls: "stun:stun3.l.google.com:19302",
+                            urls: "stun:stun4.l.google.com:19302",
+                        }
+                    ]
+                })
+
+                rtcObject.current[id].addEventListener('icecandidate', (icecandidate) => {
+                    socket.emit('ice', icecandidate.candidate, id)
+                })
+
+                rtcObject.current[id].ontrack = (e) => {
+                    if (!document.getElementById(htmlObjectId.current[id])) {
+                        var videoElement = document.createElement('video');
+                        videoElement.id = htmlObjectId.current[id];
+                        videoElement.className = 'video-self';
+                        videoElement.autoplay = true;
+                        videoElement.srcObject = e.streams[0];
+
+                        var container = document.querySelector('.video-container')
+                        container.appendChild(videoElement);
                     }
-                ]
-            })
-
-            rtcObject.current[id].addEventListener('icecandidate', (icecandidate)=>{
-                socket.emit('ice', icecandidate.candidate, id)
-            })
-
-            rtcObject.current[id].ontrack = (e)=>{
-                var videoElement = document.createElement('video');
-                videoElement.id = htmlObjectId.current[id];
-                videoElement.className = 'video-self';
-                videoElement.autoplay = true;
-                videoElement.srcObject = e.streams[0];
-                
-                var container = document.querySelector('.video-container')
-                container.appendChild(videoElement);
-                return false
+                    return false
+                }
+                myStream.current.getTracks().forEach(track => {
+                    rtcObject.current[id].addTrack(track, myStream.current)
+                });
             }
-            myStream.current.getTracks().forEach(track => {
-                rtcObject.current[id].addTrack(track, myStream.current)
-            });
-        }
-        
-        
-        async function createOffer(id) {
-            const offer = await rtcObject.current[id].createOffer()
-            rtcObject.current[id].setLocalDescription(offer);
-            socket.emit('offer', offer, id, socket.id);
-        }
 
+
+            async function createOffer(id) {
+                const offer = await rtcObject.current[id].createOffer()
+                rtcObject.current[id].setLocalDescription(offer);
+                socket.emit('offer', offer, id, socket.id);
+            }
+
+            window.addEventListener("beforeunload", handleUnLoad);
+            socket.emit('enter-rtc-room', 'WEBRTC' + id)
+        }
+        
         function handleUnLoad() {
-            socket.emit('leave-rtc', 'WEBRTC'+id)
+            socket.emit('leave-rtc', 'WEBRTC' + id)
         }
 
-        window.addEventListener("beforeunload", handleUnLoad);
-        socket.emit('enter-rtc-room', 'WEBRTC'+id)
+        start();
 
         return () => {
             window.removeEventListener("beforeunload", handleUnLoad);
@@ -143,10 +146,11 @@ export default function FaceChat (props) {
     return(
         <>
             <div className='rtc-logo-container'><p onClick={()=>{navigate('/')}}>J-Zoom</p></div>
-            <div className={soundBtn} onClick={()=>{
+            <div className={sound ? 'sound-btn' : 'mute-btn'} onClick={()=>{
+                setSound((sound)=>{return !sound})
                 myStream.current.getAudioTracks().forEach((track)=>{track.enabled = !track.enabled})
             }}></div>
-            <div className='mute-btn' onClick={()=>{
+            <div className='red-btn' onClick={()=>{
                 myStream.current.getVideoTracks().forEach((track)=>{track.enabled = !track.enabled})
             }}></div>
             <div className='rtc-flex-option-container'>
@@ -158,7 +162,6 @@ export default function FaceChat (props) {
             <div className='video-container'>
                 <video autoPlay ref={videoSelf} className='video-self'></video>
             </div>
-            <div onClick={()=>{console.log(sound)}}>클릭</div>
         </>
     )
 } 
